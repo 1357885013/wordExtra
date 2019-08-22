12c3f0d94bb88274ceab5091849ec88ed849993f
LiuJiJiang <1357885013@qq.com>
2019-08-22 20:07:56 +0800

    Update Extra.java

--- wordExtra/src/extra/Extra.java
+++ wordExtra/src/extra/Extra.java
@@ -2,6 +2,8 @@ package extra;
 
 import util.ConnectionPool;
 
+import javax.sql.rowset.serial.SerialArray;
+import java.sql.Array;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -31,7 +33,10 @@ public class Extra {
                 p = ConnectionPool.getOne();
                 ResultSet res = null;
                 try {
+                    //p.getConnection().createArrayOf("String",new String[]{"123","123"});
                     p.setString(1, word);
+                    int[] aaa = new int[] {1,2,3,44,5};
+                   //todo: p.setArray(1,new SerialArray(aaa));
                     long timeBegin = System.nanoTime();
                     res = p.executeQuery();//查询一次耗时40ms左右   加unique，btree索引后耗时0.075912ms
                     System.out.println("6System.nanoTime()-timeBegin = " + ((System.nanoTime() - timeBegin) / 1000000F));
